2743fb7faee058daad169ef2f65d72d97c3e6951
jason zhou <534320302@qq.com>
2024-04-29 17:47:57 +0800

    feat: update

--- app/middleware/http_proxy.js
+++ app/middleware/http_proxy.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxyFun = require('http-proxy-middleware');
 const c2k = require('koa2-connect');
 const pathMatching = require('egg-path-matching');
 const omit = require('lodash/omit');
+
+const proxy = proxyFun instanceof Function ? proxyFun : proxyFun.createProxyMiddleware;
 
 module.exports = options => {
   return async function httpProxy(ctx, next) {
@@ -19,7 +21,7 @@ module.exports = options => {
         if (typeof proxyOptions === 'string') {
           proxyOptions = { target: proxyOptions };
         }
-        c2k(createProxyMiddleware(context, proxyOptions))(ctx, next);
+        c2k(proxy(context, proxyOptions))(ctx, next);
       }
       return isMatch;
     });
